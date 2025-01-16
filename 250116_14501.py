771fe8477e6f15d3e9d5354077bc6966d718e764
hyunseo <ols3040015@naver.com>
2025-01-16 15:57:09 +0900

    14501 answer

--- 250116_14501.py
+++ 250116_14501.py
@@ -6,20 +6,30 @@ Created on Thu Jan 16 14:36:27 2025
 @author: hyunseo
 """
 
-#14501 Resign
+#14501 Resign 퇴사
 
-def benefit ():
-    return 0
+def benefit (data):
+    length = len(data)
+    dp = [0] * (length + 2) # Create an array of size n+2 to consider up to n+1 
+    
+    for i in range(length, 0, -1): # Calculate max profit in reverse order starting from the last day
+        t, p = data[i - 1] # Duration and revenue for consultation i
+    
+        # Check consultation availability and update DP
+        if (i + t - 1) <= length:
+            dp[i] = max(p + dp[i + t], dp[i + 1]) #
+        else:
+            dp[i] = dp[i + 1]
+        
+    print(dp)
+    return dp[1] # DP[1] stores the maximum profit you could have earned if you started on day 1
     
     
 n = int(input())
 
-data = []
+data = [] #tuple list
 for _ in range(n):
     t, p = map(int, input().split())
     data.append((t, p))
 
-# check
-print(data)
-
-print(benefit(data))
+print(benefit(data)) 
